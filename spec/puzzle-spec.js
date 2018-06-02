92961aeed66d726903be66dcfb9d0165b43c423c
silvio-galli <silvio.galli@gmail.com>
2018-06-02 20:52:38 +0200

    Added a couple of specs to test createTiles() and the propertues of the tiles

--- spec/puzzle-spec.js
+++ spec/puzzle-spec.js
@@ -17,26 +17,34 @@ describe( "Puzzle", function() {
     } );
 
     it( "this.cursor.y  === 0", function() {
-      expect( this.cursor.y ).toEqual( 0 );
+      expect( game.cursor.y ).toEqual( 0 );
     } );
 
     it( "this.cursor.x  === 0", function() {
-      expect( this.cursor.x ).toEqual( 0 );
+      expect( game.cursor.x ).toEqual( 0 );
     } );
 
   } );
 
-  describe(" >> methods >>", function() {
+  describe(" >> createTiles() >>", function() {
     
     beforeEach( function() {
-      game.tiles = [
-        { name: "tile-1", y: 0, x: 0, currentY: 0, currentX: 0, backgroundY: 0, backgroundX: 0, },
-        { name: "tile-2", y: 0, x: 1, currentY: 0, currentX: 1, backgroundY: 0, backgroundX: 1, },
-        { name: "tile-3", y: 0, x: 2, currentY: 0, currentX: 2, backgroundY: 0, backgroundX: 2, },
-        { name: "tile-4", y: 0, x: 3, currentY: 0, currentX: 3, backgroundY: 0, backgroundX: 3, },
-        { name: "tile-5", y: 0, x: 4, currentY: 0, currentX: 4, backgroundY: 0, backgroundX: 4, },
-      ]
+      game = new Puzzle();
+      game.createTiles();
     } );
+
+    it( "creates an array of 25 objects", function() {
+      expect( game.tiles.length ).toEqual( 25 );
+    } );
+
+    it( "this.tiles array contains only objects", function() {
+      expect( game.tiles.every( x => typeof x === 'object') ).toBeTruthy();
+    } );
+
+    it("every object inside this.tiles has the expected properties", function() {
+      var expected = ['name', 'y', 'x', 'currentY', 'currentX','backgroundY', 'backgroundX'].sort();
+      game.tiles.every( tile => expect( Object.keys( tile ).sort() ).toEqual( expected ) );
+    })
 
   } );
 
