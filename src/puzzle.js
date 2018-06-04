1c993889b6112939b005b30edf7cbbce428953aa
silvio-galli <silvio.galli@gmail.com>
2018-06-04 19:20:59 +0200

    Display the shuffled tiles on the page

--- src/puzzle.js
+++ src/puzzle.js
@@ -1,39 +1,51 @@
-function Puzzle() {
+function Puzzle( n = 3 ) {
+  this.level = n; // the higher the number, the higher the difficult
+                  // 3,4,5 will be the different levels
+  this.board = [];
   // collect the tiles properties inside an array of objects
   this.tiles = [];
   // the cursor vale is an object that stores its coordinates
   this.cursor = {
-    y: 0,
-    x: 0,
+    y: this.level - 1,
+    x: this.level - 1,
   }
 }
 
+
 // inside createTiles() method we instatiate anonymous objects
 // that will represent the tiles of our puzzle
-Puzzle.prototype.createTiles = function () {          // #TODO: remember that 3 can be a input from the user
-  for (var row = 0; row < 3; row++) {                       // or substituted by different levels of difficulty 
-    for (var col = 0; col < 3; col++) {
+Puzzle.prototype.createTiles = function () {
+  if ( this.tiles.length > 0 ) {
+    console.log( "You have already a bunch of tiles. Cannot continue with this operation..." );
+    return;
+  }
+  for (var row = 0; row < this.level; row++) { 
+    for (var col = 0; col < this.level; col++) {
       // here is building of the anonymous 'tile' object 
       this.tiles.push( {
-        name: 'tile' + row + col,
+        name: row + "-" + col,
         // starting position of the tile onto the board
         y: row,
         x: col,
         // below the current position of the tile onto the board
         currentY: row,
         currentX: col,
+        // tiles are squares we store the size of its side
+        side: 600 / this.level,
         // bgY and bgX are used to assign the correct portion of the
         // image as a background of the tile
-        bgY: ( 600 / 3 ) * (row === 0 ? 0 : -row), 
-        bgX: ( 600 / 3 ) * (col === 0 ? 0 : -col),
+        bgY: ( 600 / this.level ) * (row === 0 ? 0 : -row), 
+        bgX: ( 600 / this.level ) * (col === 0 ? 0 : -col),
       } );
     }
   }
 };
 
+// shuffleTiles() method shuffles tiles coordinates
+// so that they will be randomly dislpayed on the game board
 Puzzle.prototype.shuffleTiles = function () {
   // retrieve starting coordinates from the object
-  var startingTilesCoordinates = this.tiles.map( tile => tile.x + "," + tile.y );
+  var startingTilesCoordinates = this.tiles.map( tile => [ tile.x, tile.y ] );
   var shuffledTilesCoordinates = [];
   // shuffle the starting coordinates inside shuffledTilesCoordinates
   while ( startingTilesCoordinates.length > 0 ) {
@@ -44,17 +56,32 @@ Puzzle.prototype.shuffleTiles = function () {
   
   // reassigning shuffled coordinates to tiles
   for ( var i = 0; i < shuffledTilesCoordinates.length; i++) {
-    this.tiles[ i ].currentY = shuffledTilesCoordinates[ i ].split(',')[ 0 ];
-    this.tiles[ i ].currentX = shuffledTilesCoordinates[ i ].split(',')[ 1 ];
+    this.tiles[ i ].currentY = shuffledTilesCoordinates[ i ][ 0 ];
+    this.tiles[ i ].currentX = shuffledTilesCoordinates[ i ][ 1 ];
   }
 };
 
-// function Tile (name, y, x) {
-//   this.name = name;
-//   this.y = y;
-//   this.x = x;
-//   this.currentY;
-//   this.currentX;
-//   this.bg = "";
-// }
+function selectTilesPerRow( tiles, index ) {
+  return tiles.filter( function( tile ) {
+    return tile.currentY === index;
+  } )
+  .sort( function( a, b ) {
+    return a.currentX - b.currentX;
+  } );
+}
 
+// -------------- UPDATE BOARD -------------
+Puzzle.prototype.updateBoard = function () {
+  this.board = [];
+  for ( var i = 0; i < this.level; i++ ) {
+    this.board.push( selectTilesPerRow( this.tiles, i ) );
+  }
+};
+
+// check if in every object of the Puzzle.tiles array
+// y and x properties equals currentY and currentX
+Puzzle.prototype.checkCompleted = function () {
+  return this.tiles.every( function( tile ) {
+    return tile.y === tile.currentY && tile.x === tile.currentX;
+  });
+};
